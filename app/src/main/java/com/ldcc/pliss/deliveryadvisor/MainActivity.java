1ed0d24a6928cefcf9f90ecc5f3fc8b2ab881fde
Pliss <aboutdh89@naver.com>
2018-02-20 16:19:57 +0900

    앱 아이콘 생성 및 앱 최초 실행 설정

--- app/src/main/java/com/ldcc/pliss/deliveryadvisor/MainActivity.java
+++ app/src/main/java/com/ldcc/pliss/deliveryadvisor/MainActivity.java
@@ -1,13 +1,33 @@
 package com.ldcc.pliss.deliveryadvisor;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ldcc.pliss.deliveryadvisor.page.HomeActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    public SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences("Pref", MODE_PRIVATE);
+    }
+
+    public void checkFirstRun(){
+        boolean isFirstRun = prefs.getBoolean("isFirstRun",true);
+        if(isFirstRun)
+        {
+            Intent newIntent = new Intent(this, HomeActivity.class);
+            startActivity(newIntent);
+
+            prefs.edit().putBoolean("isFirstRun",false).apply();
+        }
     }
 }
